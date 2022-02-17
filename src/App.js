eeaf2563919dec2099b26cd3f93402c56ae91c2f
Brian Johnson <johnson8727@gmail.com>
2022-02-16 22:43:25 -0500

    added project files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +16,17 @@ function App() {
           Learn React
         </a>
       </header>
+      <div className="attribution">
+        Challenge by{" "}
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        &nbsp; Coded by <a href="https://brianbjohnson.net/">Brian Johnson</a>.
+      </div>
     </div>
   );
 }
